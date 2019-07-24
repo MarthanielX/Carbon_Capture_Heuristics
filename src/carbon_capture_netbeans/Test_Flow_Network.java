0fc7c0bb3059da6ff6a2102a307d0d349638a0c5
nathaniel.saverberg <j33m215@bz-cs-cslab8l.msu.montana.edu>
2019-07-24 16:07:25 -0600

    started writing tests for the flow network

---
+++ src/carbon_capture_netbeans/Test_Flow_Network.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carbon_capture_netbeans;
+
+/**
+ *
+ * @author sauerberg
+ */
+public class Test_Flow_Network {
+    
+    public static void Main(String[] args){
+        
+        double[] cap = new double[]{50, 200, 500, 1000};
+        double[] fc = new double[]{75, 150, 300, 550};
+        double[] vc = new double[]{1, .8, .6, .4};
+        
+        
+    }
+    
+}
